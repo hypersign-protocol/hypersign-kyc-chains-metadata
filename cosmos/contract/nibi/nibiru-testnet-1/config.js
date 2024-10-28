ae43d4dab34a68da8673162a5fcc882fd855c8e8
vishwas1 <vishwasanand.cse@gmail.com>
2024-10-28 12:02:11 +0530

    deployed new contract and updated config and schemas

--- cosmos/contract/nibi/nibiru-testnet-1/config.js
+++ cosmos/contract/nibi/nibiru-testnet-1/config.js
@@ -1,6 +1,6 @@
-export const HYPERSIGN_KYC_FACTORY_CODE_ID = 1312 //34
-export const ISSUER_KYC_CODE_ID = 1311
+export const HYPERSIGN_KYC_FACTORY_CODE_ID = 1359 //redundatan
+export const ISSUER_KYC_CODE_ID = 1358
 export const SBT_TOKEN_CODE_ID = 1313
-export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "nibi19c3tpkj9dr3wakehywh8mxkua56d0l5nrkzx35uyt6jxuy686yrqlr6q2u" //"nibi1f5djultkcmtxwyyadkjjjjmcncxf5yxz5qkz4qfjnkwqggrw7pdqe27m2h" //"nibi1yatzc54ln59caxxnj53rff2s359pezx3hqxpzu2tkyl2f9ud9yvsq60lle"
+export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "nibi1ghtmzya95j9tqpkww6mlvy7pvr9qfks5rp34p3pe27cahja9ft6sgz2eje" //"nibi1f5djultkcmtxwyyadkjjjjmcncxf5yxz5qkz4qfjnkwqggrw7pdqe27m2h" //"nibi1yatzc54ln59caxxnj53rff2s359pezx3hqxpzu2tkyl2f9ud9yvsq60lle"
 
 
