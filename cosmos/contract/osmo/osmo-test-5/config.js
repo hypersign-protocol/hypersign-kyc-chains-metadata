e2c0fa262fcfc6d94c750382d8e8b2d59b74d514
vishwas1 <vishwasanand.cse@gmail.com>
2024-10-29 21:13:05 +0530

    updated contract for osmosis

--- cosmos/contract/osmo/osmo-test-5/config.js
+++ cosmos/contract/osmo/osmo-test-5/config.js
@@ -1,6 +1,6 @@
-export const HYPERSIGN_KYC_FACTORY_CODE_ID = 11159
-export const ISSUER_KYC_CODE_ID = 11158
+export const HYPERSIGN_KYC_FACTORY_CODE_ID = 11320
+export const ISSUER_KYC_CODE_ID = 11319
 export const SBT_TOKEN_CODE_ID = 11160
-export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "osmo1enn2akshmm20j0rjv0ualgujnct0j7usn4slyn7esx50xkw3v57q0xjewd" //"nibi1f5djultkcmtxwyyadkjjjjmcncxf5yxz5qkz4qfjnkwqggrw7pdqe27m2h" //"nibi1yatzc54ln59caxxnj53rff2s359pezx3hqxpzu2tkyl2f9ud9yvsq60lle"
+export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "osmo1c6cgjpzpyjm5660cutptetgsvqqj4n4jtnejk9gh9zkuap346zkq3ayl88" //"nibi1f5djultkcmtxwyyadkjjjjmcncxf5yxz5qkz4qfjnkwqggrw7pdqe27m2h" //"nibi1yatzc54ln59caxxnj53rff2s359pezx3hqxpzu2tkyl2f9ud9yvsq60lle"
 
 
