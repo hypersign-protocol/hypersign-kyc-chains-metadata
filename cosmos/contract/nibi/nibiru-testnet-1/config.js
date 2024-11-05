69930a0e76a0709549d469d529556efca083a6d8
vishwas1 <vishwasanand.cse@gmail.com>
2024-11-05 17:20:54 +0530

    update nibiru contract

--- cosmos/contract/nibi/nibiru-testnet-1/config.js
+++ cosmos/contract/nibi/nibiru-testnet-1/config.js
@@ -1,6 +1,6 @@
-export const HYPERSIGN_KYC_FACTORY_CODE_ID = 1362 //redundatan
-export const ISSUER_KYC_CODE_ID = 1361
-export const SBT_TOKEN_CODE_ID = 1313
-export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "nibi1yv3yzs5n90a7zxnqlndrve57s0ntz5kd6k39dvswg8egmnryyf3sdagrnr"
+export const HYPERSIGN_KYC_FACTORY_CODE_ID = 1394 //redundatan
+export const ISSUER_KYC_CODE_ID = 1393
+export const SBT_TOKEN_CODE_ID = 1395
+export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "nibi1zayxxsz5l9m0l058t9ckud6jdxenu09qstaplf896wy4ux9e3xnsxxu9ps"
 
 
