41360756e3961081f3b65b3c991f6a48cdd5ae10
Pratap2018 <pratapmridha@gmail.com>
2025-01-21 20:29:31 +0530

    add luna

---
+++ cosmos/contract/terra/rebel-2/config.js
@@ -0,0 +1,6 @@
+export const HYPERSIGN_KYC_FACTORY_CODE_ID = 1230
+export const ISSUER_KYC_CODE_ID = 1229
+export const SBT_TOKEN_CODE_ID = 1231
+export const HYPERSIGN_KYC_FACTORY_CONTRACT_ADDRESS = "terra195vs9e4cn0arm4lsj5dwp97rl3s06stys7hfxslzwdkg5cjh4ydspxy4yl" //"nibi1f5djultkcmtxwyyadkjjjjmcncxf5yxz5qkz4qfjnkwqggrw7pdqe27m2h" //"nibi1yatzc54ln59caxxnj53rff2s359pezx3hqxpzu2tkyl2f9ud9yvsq60lle"
+
+
